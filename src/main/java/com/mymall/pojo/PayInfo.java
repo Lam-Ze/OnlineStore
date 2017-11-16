a792e1589886518aa8f816a447cb4194db3d9990
lamZe <673388164@qq.com>
2017-11-17 03:09:43 +0800

    project init commit

---
+++ src/main/java/com/mymall/pojo/PayInfo.java
@@ -0,0 +1,100 @@
+package com.mymall.pojo;
+
+import java.util.Date;
+
+public class PayInfo {
+    private Integer id;
+
+    private Integer userId;
+
+    private Long orderNo;
+
+    private Integer payPlatform;
+
+    private String platformNumber;
+
+    private String latformStatus;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public PayInfo(Integer id, Integer userId, Long orderNo, Integer payPlatform, String platformNumber, String latformStatus, Date createTime, Date updateTime) {
+        this.id = id;
+        this.userId = userId;
+        this.orderNo = orderNo;
+        this.payPlatform = payPlatform;
+        this.platformNumber = platformNumber;
+        this.latformStatus = latformStatus;
+        this.createTime = createTime;
+        this.updateTime = updateTime;
+    }
+
+    public PayInfo() {
+        super();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Long getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(Long orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    public Integer getPayPlatform() {
+        return payPlatform;
+    }
+
+    public void setPayPlatform(Integer payPlatform) {
+        this.payPlatform = payPlatform;
+    }
+
+    public String getPlatformNumber() {
+        return platformNumber;
+    }
+
+    public void setPlatformNumber(String platformNumber) {
+        this.platformNumber = platformNumber == null ? null : platformNumber.trim();
+    }
+
+    public String getLatformStatus() {
+        return latformStatus;
+    }
+
+    public void setLatformStatus(String latformStatus) {
+        this.latformStatus = latformStatus == null ? null : latformStatus.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
